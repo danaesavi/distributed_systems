d9a06b50a4477abb49bb94dd5f2db05d55b0259f
Danae <danaesavi@gmail.com>
2016-09-13 23:42:18 -0500

    SOAP- web services

---
+++ WebApplicationWebServices/src/java/webservices/MyWebService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservices;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Danae
+ */
+@WebService(serviceName = "MyWebService")
+@Stateless()
+public class MyWebService {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "a") int a, @WebParam(name = "b") int b) {
+
+        return a+b;
+    }
+}
+
+
