d03b3df24e4f1bbe0d964f36019176c635e3d09c
Danae <danaesavi@gmail.com>
2016-09-13 23:32:19 -0500

    JASON and AJAX

---
+++ JSON_AJAX/WebApplicationJSON1/src/java/servlets/ServletJSON1.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author hca
+ */
+public class ServletJSON1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+//            JSONObject  jsonResponse  = new JSONObject();
+//
+//            jsonResponse.put("field1", "value1");
+//            jsonResponse.put("field2", "value2");
+//            jsonResponse.put("field3", "value3");
+//
+//            response.setContentType("application/json");
+//
+//            out.write(jsonResponse.toString());
+            JSONObject jsonResponse = new JSONObject();
+
+            JSONArray  addresses = new JSONArray();
+
+            for (int i=0 ; i<10; i++) {
+               JSONObject address = new JSONObject();
+               address.put("name"  , "Decepticon " + i);
+               address.put("id" , i);
+               addresses.add(address);
+            }
+
+            jsonResponse.put("addresses", addresses);
+            response.setContentType("application/json");
+            out.write(jsonResponse.toString());
+
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+
